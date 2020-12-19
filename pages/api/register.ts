7ce5c2deea6ca712025d9da6867aa57247043544
ThePez <santiagodavidlopezbenavides@gmail.com>
2020-12-19 14:44:36 -0500

    register fixed

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -32,7 +32,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           email: req.body.email,
           password: req.body.password,
           dateOfBirth: req.body.dateOfBirth,
-          admin: true
+          admin: false
         })
 
         user.password = await user.encryptPassword(user.password)
@@ -41,7 +41,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           { _id: newUser._id },
           process.env.TOKEN_SECRET,
           {
-            // expiresIn: 60 * 60 * 24
+            expiresIn: 60 * 60 * 24
           }
         )
         res.status(201).json(
