a008595d300d61022528b84e893f02c4e148c2c3
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-03-06 15:03:49 -0500

    fix: card should not be editable

--- pages/api/card/[id].ts
+++ pages/api/card/[id].ts
@@ -4,7 +4,6 @@ import Config from 'lib/config'
 import User, { IUser } from 'lib/database/models/user'
 import Card, { ICard } from 'lib/database/models/card'
 
-import validation from 'lib/validation/card'
 import AdminError from 'lib/error/admin'
 
 export default async (
@@ -14,29 +13,13 @@ export default async (
   try {
     await Config({
       req,
-      method: ['GET', 'PUT', 'DELETE'],
-      auth: [false, true, true],
+      method: ['GET', 'DELETE'],
+      auth: [false, true],
     })
 
     const profile: IUser = await User.findById(req.body._id)
 
     if (req.method === 'GET') {
-      const card: ICard = await Card.findById(req.query.id as string)
-
-      res.status(200).json({
-        success: true,
-        card,
-      })
-    } else if (req.method === 'PUT') {
-      if (!profile.admin)
-        throw new AdminError('You need to be an admin to access')
-
-      const update = req.body.update || req.body
-      delete update._id
-
-      await validation.validateAsync(update)
-
-      await Card.updateOne({ _id: req.query.id as string }, { ...update })
       const card: ICard = await Card.findById(req.query.id as string)
 
       res.status(200).json({
